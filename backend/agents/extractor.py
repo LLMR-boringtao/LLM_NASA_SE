5e287493de0769d78b844da6f0633b8f7bc75de2
LLMR_Boringtao <138439682+LLMR-boringtao@users.noreply.github.com>
2024-06-02 12:55:40 +0100

    add: extrator 4v

--- backend/agents/extractor.py
+++ backend/agents/extractor.py
@@ -4,10 +4,32 @@ from typing_extensions import Annotated
 import autogen
 from autogen import AssistantAgent
 from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+from autogen.agentchat.contrib.multimodal_conversable_agent import MultimodalConversableAgent
 
 config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
+llm_config = {
+    "config_list": config_list,
+}
+extractor = MultimodalConversableAgent(
+    name="extractor",
+    max_consecutive_auto_reply=5,
+    llm_config=llm_config,
+)
 
-print("LLM models: ", [config_list[i]["model"] for i in range(len(config_list))])
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    system_message="I am a user proxy agent",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    human_input_mode="TERMINATE",
+    max_consecutive_auto_reply=0,
+)
+
+message = """
+Extract the information from the user's image input.
+<img /home/ros2/Projects/LLM_NASA/backend/agents/data/invoice.jpg>.
+"""
+
+user_proxy.initiate_chat(extractor, message=message)
 
 class Extractor:
     def __init__(self, request, context=""):

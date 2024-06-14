4e0ddaa926f6906c90f1525c3c298fa2e509ba2e
LLMR_Boringtao <138439682+LLMR-boringtao@users.noreply.github.com>
2024-06-14 16:06:01 +0100

    Integrated autogen library and added new route for running tasks in Flask app.

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
-from flask import Flask, render_template, redirect, request
+import autogen
+from autogen import AssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+from autogen.agentchat.contrib.multimodal_conversable_agent import MultimodalConversableAgent
+import json
 
+config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
+llm_config = {
+    "config_list": config_list,
+    "max_tokens": 4000,
+    "seed": 42,
+    "temperature": 0
+}
+assistant = AssistantAgent(
+    name="assistant",
+    llm_config=llm_config
+)
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    llm_config=llm_config,
+    human_input_mode="NEVER",
+    system_message="Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved yet.",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    max_consecutive_auto_reply=0,
+    code_execution_config={
+        "use_docker": False,
+    }
+)
+
+groupchat=autogen.GroupChat(agents=[assistant, user_proxy], messages=[])
+manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
+from flask import Flask, jsonify, render_template, redirect, request
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/run", methods=["GET", "POST"])
+def run():
+    data = request.get_json()
+    task = data["message"]
+    user_proxy.initiate_chat(manager, message=f"how to {task}?")
+
+    messages = user_proxy.chat_messages[manager]
+    return jsonify(messages)
+
